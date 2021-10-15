bd649f23e07ab529f8e989c1fbd2799cee4b27fe
Bernard van der Helm <bernard.van.der.helm@startmail.com>
2021-10-15 14:32:07 +0200

    smarter

--- token.sh
+++ token.sh
@@ -1,3 +1,1 @@
-KD=$(kubectl get secret | grep dashboard-admin-sa-token | awk '{print $1}')
-kubectl get secret $KD -o go-template='{{.data.token | base64decode}}'
-echo
+echo $(kubectl get secret $(kubectl get secret | grep dashboard-admin-sa-token | awk '{print $1}') -o go-template='{{.data.token | base64decode}}')
